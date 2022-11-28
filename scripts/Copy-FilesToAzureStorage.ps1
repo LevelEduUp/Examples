9e6ab2247ed57c9b528a4b7db1244267656fe921
Masimaka <maxateambel@gmail.com>
2022-11-28 16:48:53 +0300

    adc create container sytep

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -1,6 +1,16 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ContainerName
+)
+
 for ($i = 0; $i -lt 10; $i++) {
     New-Item -Name "$i.txt" 
 }
+$storage = Get-AzStorageContainer -Name "levelupexample"
+
+New-AzStorageContainer -Name $ContainerName -Context $storage.Context -Permission Blob
 
 foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
     $item
