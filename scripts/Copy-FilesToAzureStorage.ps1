ce1e7891facb1d3792eb54558d37d2e5c702291c
Masimaka <maxateambel@gmail.com>
2022-11-28 17:33:31 +0300

    remove txt file

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -10,13 +10,13 @@ param (
 
 )
 
-for ($i = 0; $i -lt 1; $i++) {
+for ($i = 0; $i -lt 10; $i++) {
     New-Item -Name "$i.txt" 
 }
 $Container = $ContainerName.Replace(" ", "").ToLower()
 az storage container create --account-name $accountname -n $Container --public-access blob --auth-mode login
 
-foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
+foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt999"})) {
     $item.Name
     az storage blob upload -f "$item" -n $item.Name -c $Container --account-name $accountname  
 }
