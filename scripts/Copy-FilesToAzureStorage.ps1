36ad50c962d0d8e7eaf24e97ec93c4af7c31e7d0
Masimaka <maxateambel@gmail.com>
2022-11-28 17:51:07 +0300

    add skip step

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -10,13 +10,22 @@ param (
 
 )
 
+# Generating files step
 for ($i = 0; $i -lt 10; $i++) {
     New-Item -Name "$i.txt" 
 }
-$Container = $ContainerName.Replace(" ", "").ToLower()
-az storage container create --account-name $accountname -n $Container --public-access blob --auth-mode login
 
-foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
-    $item.Name
-    az storage blob upload -f "$item" -n $item.Name -c $Container --account-name $accountname  
+$files = $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})
+
+if ($files.count -gt 0) {
+    Write-Output "new files found"
+    $Container = $ContainerName.Replace(" ", "").ToLower()
+    az storage container create --account-name $accountname -n $Container --public-access blob --auth-mode login
+    foreach ($item in $files) {
+        $item.Name
+        az storage blob upload -f "$item" -n $item.Name -c $Container --account-name $accountname  
+    }
+}
+else {
+    Write-Output "no files - skip"
 }
