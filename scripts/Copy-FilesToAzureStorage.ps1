0d41caff2dd16003a516c185f4464f987255d242
Masimaka <maxateambel@gmail.com>
2022-11-28 17:07:16 +0300

    create with cli

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -8,8 +8,7 @@ param (
 for ($i = 0; $i -lt 10; $i++) {
     New-Item -Name "$i.txt" 
 }
-Get-Module -ListAvailable -Name Az.Accounts
-
+az storage container create --account-name "levelupexample" -n $ContainerName --public-access blob
 #$storage = Get-AzStorageContainer -Name "levelupexample"
 #az storage container create -n mystoragecontainer --public-access blob
 
@@ -18,3 +17,5 @@ Get-Module -ListAvailable -Name Az.Accounts
 foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
     $item
 }
+
+az storage account list -g 'LevelEduup_sandbox'
