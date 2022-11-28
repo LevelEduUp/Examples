2fe6b0557600060b684be98faccf03579d43ed77
Masimaka <maxateambel@gmail.com>
2022-11-28 17:38:05 +0300

    remove txt filter

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -16,7 +16,7 @@ for ($i = 0; $i -lt 10; $i++) {
 $Container = $ContainerName.Replace(" ", "").ToLower()
 az storage container create --account-name $accountname -n $Container --public-access blob --auth-mode login
 
-foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt999"})) {
+foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
     $item.Name
     az storage blob upload -f "$item" -n $item.Name -c $Container --account-name $accountname  
 }
