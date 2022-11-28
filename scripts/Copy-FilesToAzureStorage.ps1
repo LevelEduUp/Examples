8c225ebf458dfaf34837576b20dccf91ec048fc7
Masimaka <maxateambel@gmail.com>
2022-11-28 17:14:14 +0300

    add workflow to name

--- scripts/Copy-FilesToAzureStorage.ps1
+++ scripts/Copy-FilesToAzureStorage.ps1
@@ -8,14 +8,8 @@ param (
 for ($i = 0; $i -lt 10; $i++) {
     New-Item -Name "$i.txt" 
 }
-az storage container create --account-name "levelupexample" -n $ContainerName --public-access blob
-#$storage = Get-AzStorageContainer -Name "levelupexample"
-#az storage container create -n mystoragecontainer --public-access blob
-
-#New-AzStorageContainer -Name $ContainerName -Context $storage.Context -Permission Blob
+az storage container create --account-name "levelupexample" -n $ContainerName.Replace(" ", "").ToLower() --public-access blob --auth-mode login
 
 foreach ($item in $(Get-ChildItem | Where-Object {$_.Name -like "*.txt"})) {
     $item
 }
-
-az storage account list -g 'LevelEduup_sandbox'
